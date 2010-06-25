4ed359ff577a7813fc22e539a64df506bb61ab36
James S <i@nixeagle.org>
2010-06-25 01:45:05 +0000

    Hitting RET on a required file in a usefile will open the named file.

--- reincarnate/ooc-usefile-mode.el
+++ reincarnate/ooc-usefile-mode.el
@@ -8,7 +8,7 @@
 ;; Version:
 ;; Last-Updated:
 ;;           By:
-;;     Update #: 5
+;;     Update #: 7
 ;; URL:
 ;; Keywords:
 ;; Compatibility:
@@ -91,15 +91,68 @@ above them"
   :group 'ooc-usefile-button
   :type 'regexp)
 
+(defvar ooc-usefile-button-keymap
+  (let ((map (make-sparse-keymap)))
+    (define-key map (kbd "RET") 'ooc-usefile-button-press-button)
+    map)
+  "Local keymap for ooc usefile buttons.")
+
 (defun ooc-library-expand-file-name (filename)
   "Expand FILENAME as an ooc-library."
   (expand-file-name (concat (file-name-sans-extension filename) ".use")
                     ooc-library-path))
 
+(defun ooc-button-add-button (from to face map)
+  (add-text-properties from to `(mouse-face ,face
+                                            keymap ,map
+                                            rear-nonsticky t)))
+
+;; From erc-button.el `erc-button-add-face' [2010-06-24 Thu 23:08]
+(defun ooc-button-add-face (from to face)
+  "Add FACE to the region between FROM and TO."
+  (let ((old (if (listp (get-text-property from 'face))
+                 (get-text-property from 'face)
+               (list (get-text-property from 'face))))
+        (pos from)
+        (end (next-single-property-change from 'face nil to))
+        new)
+    ;; old is the face at pos, in list form.  It is nil if there is no
+    ;; face at pos.  If nil, the new face is FACE.  If not nil, the
+    ;; new face is a list containing FACE and the old stuff.  end is
+    ;; where this face changes.
+    (while (< pos to)
+      (setq new (if old (cons face old) face))
+      (put-text-property pos end 'face new)
+      (setq pos end
+            old (if (listp (get-text-property from 'face))
+                 (get-text-property from 'face)
+                 (list (get-text-property from 'face)))
+            end (next-single-property-change pos 'face nil to)))))
+
+
+(defun ooc-usefile-button-press-button ()
+  "Open requires file that text at point describes."
+  (interactive)
+  (find-file (ooc-library-expand-file-name (symbol-name (symbol-at-point)))))
+
 (defvar ooc-usefile-mode-font-lock-keywords
   `(("^#.*$" . font-lock-comment-face)
-    ("^\\([^\\#]*:\\).*"  (1 font-lock-keyword-face))
+    ("^\\([^\\#:]*:\\)"  (1 font-lock-keyword-face))
 
+    ("^Requires:" "[^\s]" nil
+     (save-excursion
+       (forward-line 0)
+       (search-forward ":")
+       (let ((.end (line-end-position))
+         (.start (point)))
+     (loop until (> (point) .end)
+           for .white = (forward-whitespace 1)
+           for .symbol = (forward-symbol 1)
+           until (> .symbol .end)
+           do (ooc-button-add-button .white .symbol
+                                     ooc-usefile-button-mouse-face
+                                     ooc-usefile-button-keymap))))
+     (0 font-lock-variable-name-face))
     ("^[^#].+$" . font-lock-warning-face)))
 
 (defvar ooc-usefile-mode-font-lock-defaults
