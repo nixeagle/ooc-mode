21589095692a30b5796fc5534abe932feb50d39a
James S <i@nixeagle.org>
2010-07-14 23:35:22 +0000

    New keyword summaries supplied by gmaster1440 (Mark Fayngersh) Thank you!

--- ooc-wisent-wy.el
+++ ooc-wisent-wy.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2010 James
 
 ;; Author: James <i@nixeagle.org>
-;; Created: 2010-07-14 22:25:59+0000
+;; Created: 2010-07-14 23:34:15+0000
 ;; Keywords: syntax
 ;; X-RCS: $Id$
 
@@ -39,6 +39,44 @@
 (defconst ooc-wisent-wy--keyword-table
   (semantic-lex-make-keyword-table
    '(("break" . BREAK_KW)
+     ("continue" . CONTINUE_KW)
+     ("return" . RETURN_KW)
+     ("func" . FUNC_KW)
+     ("class" . CLASS_KW)
+     ("cover" . COVER_KW)
+     ("enum" . ENUM_KW)
+     ("interface" . INTERFACE_KW)
+     ("from" . FROM_KW)
+     ("abstract" . ABSTRACT_KW)
+     ("final" . FINAL_KW)
+     ("static" . STATIC_KW)
+     ("inline" . INLINE_KW)
+     ("extends" . EXTENDS_KW)
+     ("extern" . EXTERN_KW)
+     ("unmangled" . UNMANGLED_KW)
+     ("implements" . IMPLEMENTS_KW)
+     ("import" . IMPORT_KW)
+     ("include" . INCLUDE_KW)
+     ("use" . USE_KW)
+     ("if" . IF_KW)
+     ("else" . ELSE_KW)
+     ("for" . FOR_KW)
+     ("true" . WHILE_KW)
+     ("match" . MATCH_KW)
+     ("case" . CASE_KW)
+     ("as" . AS_KW)
+     ("in" . IN_KW)
+     ("into" . INTO_KW)
+     ("version" . VERSION_KW)
+     ("proto" . PROTO_KW)
+     ("set" . SET_KW)
+     ("get" . GET_KW)
+     ("operator" . OPERATOR_KW)
+     ("const" . CONST_KW)
+     ("true" . TRUE_KW)
+     ("false" . FALSE_KW)
+     ("null" . NULL_KW)
+     ("break" . BREAK_KW)
      ("continue" . CONTINUE_KW)
      ("return" . RETURN_KW)
      ("func" . FUNC_KW)
@@ -116,7 +154,27 @@
      ("{" . OPEN_BRACK)
      ("}" . CLOS_BRACK)
      ("~" . TILDE))
-   '(("if" summary "if (...) { ... } [[else | else if] { ... }]")))
+   '(("if" summary "if (...) { ... } [[else | else if] { ... }]")
+     ("as" summary "TYPE as ANOTHERTYPE")
+     ("for" summary "for (NAME in [List|Range|1..10]) {...}")
+     ("if" summary "if (...) { ... } [[else | else if] { ... }]")
+     ("use" summary "use FILE(without .ooc)")
+     ("include" summary "include stdio")
+     ("import" summary "import FOLDER/FILE(without .ooc) | import FOLDER/[FILE1,FILE2]")
+     ("implements" summary "NAME: class implements INTERFACE1,INTERFACE2 {...}")
+     ("unmangled" summary "NAME: inline unmangled func {...}")
+     ("extern" summary "NAME: extern[(EXTERNFUNCNAME)] func")
+     ("extends" summary "NAME: class extends OTHERCLASSNAME {...}")
+     ("inline" summary "NAME: inline func {...}")
+     ("static" summary "NAME: static func {...}")
+     ("abstract" summary "NAME: abstract func {...} | NAME: abstract class {...}")
+     ("interface" summary "NAME: interface {...}")
+     ("cover" summary "NAME: cover [from TYPE] {...}")
+     ("class" summary "NAME: [abstract] class [extends CLASS | implements INTERFACE1, INTERFACE2] {...}")
+     ("func" summary "NAME: [extern | inline] func [~overloaded] [(PARAMS: PARAMSTYPE)] [-> RETURNTYPE] {...}")
+     ("return" summary "return VAL // called inside of a function to return a value. Last statement in function is implicit return")
+     ("continue" summary "for (i in 0..5) continue // continues the loop")
+     ("break" summary "for (i in 0..5) break // i will never get to be 1")))
   "Table of language keywords.")
 
 (defconst ooc-wisent-wy--token-table
@@ -126,8 +184,9 @@
 (defconst ooc-wisent-wy--parse-table
   (progn
     (eval-when-compile
-      (or (require 'wisent-comp nil t)
-          (require 'semantic/wisent/comp nil t)))
+      (or
+       (require 'wisent-comp nil t)
+       (require 'semantic/wisent/comp nil t)))
     (wisent-compile-grammar
      '((BREAK_KW CONTINUE_KW RETURN_KW FUNC_KW CLASS_KW COVER_KW ENUM_KW INTERFACE_KW FROM_KW ABSTRACT_KW FINAL_KW STATIC_KW INLINE_KW EXTENDS_KW EXTERN_KW UNMANGLED_KW IMPLEMENTS_KW IMPORT_KW INCLUDE_KW USE_KW IF_KW ELSE_KW FOR_KW WHILE_KW MATCH_KW CASE_KW AS_KW IN_KW INTO_KW VERSION_KW PROTO_KW SET_KW GET_KW OPERATOR_KW CONST_KW TRUE_KW FALSE_KW NULL_KW COMMA R_ARROW DOUBLE_ARROW ASS_DECL ASS_ADD ASS_SUB ASS_MUL ASS_DIV ASS_B_RSHIFT ASS_B_LSHIFT ASS_B_XOR ASS_B_OR ASS_B_AND QUEST L_OR L_AND B_XOR EQUALS NOT_EQUALS LESSTHAN MORETHAN CMP LESSTHAN_EQ MORETHAN_EQ B_LSHIFT B_RSHIFT DOUBLE_DOT L_NOT B_NOT PLUS MINUS PERCENT STAR OPEN_PAREN CLOS_PAREN OPEN_SQUAR CLOS_SQUAR OPEN_BRACK CLOS_BRACK TILDE)
        nil
